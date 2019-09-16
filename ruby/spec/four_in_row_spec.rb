30236730bf5bf287572dc4c6cd04ce88d1b0d630
Zeus <zeusperezgarcia@gmail.com>
2019-09-16 09:17:44 +0200

    Remove debugging commands

--- ruby/spec/four_in_row_spec.rb
+++ ruby/spec/four_in_row_spec.rb
@@ -4,8 +4,6 @@ describe FourInRow do
   let(:board) { CSV.read('spec/board.csv') }
 
   it 'returns the result of a given board' do
-    require 'pry'
-    binding.pry
     expect(FourInRow.result(board)).to eq :blue
   end
 end
